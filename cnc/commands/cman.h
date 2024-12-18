4e6ef3fab9c39c60e43234e03d07f600ca79dc1c
Arch64 <buldukemir1@gmail.com>
2024-12-19 00:20:13 +0300

    Update cman.h

--- cnc/commands/cman.h
+++ cnc/commands/cman.h
@@ -4,22 +4,17 @@
 
 #include <stdbool.h>
 
-// Function signature for a command handler
 typedef void (*CommandHandler)(int clientSocket, const char* args);
 
-// Structure to represent a command
 typedef struct {
     const char* name;
     CommandHandler handler;
 } Command;
 
-// Function to initialize the command handler
 void initializeCommandHandler();
 
-// Function to register a command
 bool registerCommand(const char* name, CommandHandler handler);
 
-// Function to execute a command
 void executeCommand(int clientSocket, const char* command);
 
 void getCommandCount(int* countPtr);
